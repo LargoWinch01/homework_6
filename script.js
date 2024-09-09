d3cfd0fc49de5593e94cbbe93b3aa462fa5ec967
LargoWinch01 <tokorobakidze7@gmail.com>
2024-09-09 21:21:16 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const array = [10,20,30,40,50];
 const valueToFind=30;
 
 if (array.includes(valueToFind)){
-    console.log('რიცხვი ნაპოვნია')
+    console.log('რიცხვი ნაპოვნია');
 }else{
-    console.log('რიცხვი არ მოიძებნა')
+    console.log('რიცხვი არ მოიძებნა');
 }
